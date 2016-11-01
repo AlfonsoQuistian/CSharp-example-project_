94677a7c56607143628ae9d1fd5e1227fe500efe
Alfonso Quistian <quistian@email.uscupstate.edu>
2016-10-31 20:32:33 -0400

    Saving app progress 1
    app can open and switch pages

---
+++ App2/App2/gameBoard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    class gameBoard
+    {
+        private string[,] gameBoardObj;
+        private int boardSize;
+
+        public gameBoard()
+        {
+            gameBoardObj = new string[4,4];
+            boardSize = 4;
+        }
+
+        public gameBoard(int size)
+        {
+            gameBoardObj = new string[size + 1, size + 1];
+            boardSize = size + 1;
+        }
+
+        public void initBoard(gameBoard board)
+        {
+
+        }
+
+    }
+}
